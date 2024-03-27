00847ddc57313267e81bde32856cbbe25f59abd7
Fixer <67309088+leelang7@users.noreply.github.com>
2024-03-27 17:53:04 +0900

    Update matrix_face_recognition.py

--- matrix_face_recognition.py
+++ matrix_face_recognition.py
@@ -1,120 +1,79 @@
-#!/usr/bin/env python
-# coding: utf-8
+# you can get.py through this cli : jupyter nbconvert --to script notebook.ipynb
+# .ipynb -> .py
 
-# In[1]:
+import sys
+sys.path.append('.')
 import dlib, cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import matplotlib.patheffects as path_effects
 
 detector = dlib.get_frontal_face_detector()
 sp = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 facerec = dlib.face_recognition_model_v1('./dlib_face_recognition_resnet_model_v1.dat')
 
-# In[2]:
-def find_faces(img):
+descs = np.load('img/descs.npy', allow_pickle=True)
+if descs.ndim == 0:
+    descs = descs.item()  # 딕셔너리로 변환
+
+def encode_face(img):
     dets = detector(img, 1)
 
     if len(dets) == 0:
-        return np.empty(0), np.empty(0), np.empty(0)
-    
-    rects, shapes = [], []
-    shapes_np = np.zeros((len(dets), 68, 2), dtype=np.int)
+        return np.empty(0)
+
     for k, d in enumerate(dets):
-        rect = ((d.left(), d.top()), (d.right(), d.bottom()))
-        rects.append(rect)
-
         shape = sp(img, d)
-        
-        # convert dlib shape to numpy array
-        for i in range(0, 68):
-            shapes_np[k][i] = (shape.part(i).x, shape.part(i).y)
-
-        shapes.append(shape)
-        
-    return rects, shapes, shapes_np
-
-def encode_faces(img, shapes):
-    face_descriptors = []
-    for shape in shapes:
         face_descriptor = facerec.compute_face_descriptor(img, shape)
-        face_descriptors.append(np.array(face_descriptor))
 
-    return np.array(face_descriptors)
+        return np.array(face_descriptor)
 
+video_path = 'img/matrix_reloaded_1080p.mp4'
+cap = cv2.VideoCapture(video_path)
 
-# # Compute Saved Face Descriptions
-# In[3]:
-img_paths = {
-    'choi': 'img/choi.png',
-    'you': 'img/you.png',
-    'lee': 'img/lee.png',
-    'kim': 'img/kim.png'
-}
+if not cap.isOpened():
+    exit()
 
-descs = {
-    'choi': None,
-    'you': None,
-    'lee': None,
-    'kim': None
-}
+_, img_bgr = cap.read() # (800, 1920, 3)
+padding_size = 0
+resized_width = 1920
+video_size = (resized_width, int(img_bgr.shape[0] * resized_width // img_bgr.shape[1]))
+output_size = (resized_width, int(img_bgr.shape[0] * resized_width // img_bgr.shape[1] + padding_size * 2))
 
-for name, img_path in img_paths.items():
-    img_bgr = cv2.imread(img_path)
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+writer = cv2.VideoWriter('%s_output.mp4' % (video_path.split('.')[0]), fourcc, cap.get(cv2.CAP_PROP_FPS), output_size)
+
+while True:
+    ret, img_bgr = cap.read()
+    if not ret:
+        break
+
+    img_bgr = cv2.resize(img_bgr, video_size)
     img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 
-    _, img_shapes, _ = find_faces(img_rgb)
-    descs[name] = encode_faces(img_rgb, img_shapes)[0]
+    dets = detector(img_bgr, 1)
 
-np.save('img/descs.npy', descs)
-print(descs)
+    for k, d in enumerate(dets):
+        shape = sp(img_rgb, d)
+        face_descriptor = facerec.compute_face_descriptor(img_rgb, shape)
 
+        min_dist = 999
+        min_name = 'unknown'
 
-# # Compute Input
+        for name, saved_desc in descs.items():
+            dist = np.linalg.norm(face_descriptor - saved_desc)
 
-# In[17]:
-img_bgr = cv2.imread('img/pamyo3.jpg')
-img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+            if dist < min_dist:
+                min_dist = dist
+                min_name = name
 
-rects, shapes, _ = find_faces(img_rgb)
-descriptors = encode_faces(img_rgb, shapes)
+        color = (255, 255, 255) if min_name != 'unknown' else (0, 0, 255)
+        cv2.rectangle(img_bgr, pt1=(d.left(), d.top()), pt2=(d.right(), d.bottom()), color=color, thickness=2)
+        cv2.putText(img_bgr, min_name, org=(d.left(), d.top()), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=color, thickness=2)
 
+    writer.write(img_bgr)
 
-# # Visualize Output
-# In[18]:
-fig, ax = plt.subplots(1, figsize=(20, 20))
-ax.imshow(img_rgb)
+    cv2.imshow('img', img_bgr)
+    if cv2.waitKey(1) == ord('q'):
+        break
 
-for i, desc in enumerate(descriptors):
-    
-    found = False
-    for name, saved_desc in descs.items():
-        dist = np.linalg.norm([desc] - saved_desc, axis=1)
-
-        if dist < 0.6:
-            found = True
-
-            text = ax.text(rects[i][0][0], rects[i][0][1], name,
-                    color='b', fontsize=40, fontweight='bold')
-            text.set_path_effects([path_effects.Stroke(linewidth=10, foreground='white'), path_effects.Normal()])
-            rect = patches.Rectangle(rects[i][0],
-                                 rects[i][1][1] - rects[i][0][1],
-                                 rects[i][1][0] - rects[i][0][0],
-                                 linewidth=2, edgecolor='w', facecolor='none')
-            ax.add_patch(rect)
-
-            break
-    
-    if not found:
-        ax.text(rects[i][0][0], rects[i][0][1], 'unknown',
-                color='r', fontsize=20, fontweight='bold')
-        rect = patches.Rectangle(rects[i][0],
-                             rects[i][1][1] - rects[i][0][1],
-                             rects[i][1][0] - rects[i][0][0],
-                             linewidth=2, edgecolor='r', facecolor='none')
-        ax.add_patch(rect)
-
-plt.axis('off')
-plt.savefig('result/output.png')
-plt.show()
+cap.release()
+writer.release()
